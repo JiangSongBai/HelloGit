abcd1295d715620c8f9e35ca7263ff31b0362046
JiangSongBai <1039677907@qq.com>
2016-01-06 00:04:35 +0800

    egit 2

--- src/com/atguigu/test/Hello.java
+++ src/com/atguigu/test/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!!");
 		System.out.println("Hello EGit!!!");
+		System.out.println("Hello world!!!");
 	}
 
 }
