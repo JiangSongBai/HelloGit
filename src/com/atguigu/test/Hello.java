9d18c6de2a36ad4f69726fbc80d7e00e1912f241
JiangSongBai <JiangSongBai@users.noreply.github.com>
2016-01-06 00:08:05 +0800

    hello eclipse

--- src/com/atguigu/test/Hello.java
+++ src/com/atguigu/test/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!!");
-		System.out.println("Hello EGit!!!");
+		System.out.println("Hello Eclipse!!!");
 		System.out.println("Hello world!!!");
 		System.out.println("Hello BeiJing!!!");
 	}
